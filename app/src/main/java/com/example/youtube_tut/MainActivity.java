02efcb8552da077f208617fc728a99850aac2e4a
MariaCojocaru <mariacojocaru917@gmail.com>
2021-10-22 18:42:51 +0300

    pin app

--- app/src/main/java/com/example/youtube_tut/MainActivity.java
+++ app/src/main/java/com/example/youtube_tut/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.youtube_tut;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,27 +8,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-private FloatingActionButton btn;
+private Button btn;
+private TextView txt;
+private final int MainActivityRequestCode=100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        btn=findViewById(R.id.floatingActionButton);//cauta butonul in
+        txt=findViewById(R.id.editTextTextEmailAddress2);
+        btn=findViewById(R.id.bt1);//cauta butonul in
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,"apasat pe buton",Toast.LENGTH_LONG).show();
-                Intent intent=new Intent(getApplicationContext(),MainActivity2.class);
-                startActivity(intent);
-
+//                Intent intent=new Intent(getApplicationContext(),MainActivity2.class);
+//
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivityForResult(intent,MainActivityRequestCode);
             }
+
+
+
 
         });
 //        Intent myIntent = new Intent(MainActivity.this, MainActivity2.class);
@@ -36,7 +45,20 @@ private FloatingActionButton btn;
 //
         Log.v("lifestyle","onCreate()");
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==MainActivityRequestCode){
+            if(resultCode==RESULT_OK){
+                if(data!=null){
+                    Bundle bundle=data.getBundleExtra("bundle");
+                    Utilizator u= bundle.getParcelable("OK");
+                    txt.setText(u.getEmail().toString());
 
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/youtube_tut/MainActivity2.java
+++ app/src/main/java/com/example/youtube_tut/MainActivity2.java
@@ -2,13 +2,91 @@ package com.example.youtube_tut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity2 extends AppCompatActivity {
-
+Utilizator utilizator;
+Button Login;
+EditText nume, prenume, data, email,parola;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        //preluare date de la utilizator
+        utilizator=new Utilizator();
+        //initializare campuri
+        Login=findViewById(R.id.button);
+        nume=findViewById(R.id.nume);
+        prenume=findViewById(R.id.Prenume);
+        data=findViewById(R.id.editTextDate);
+        email=findViewById(R.id.editTextTextEmailAddress);
+        parola=findViewById(R.id.editTextTextPassword);
+
+        //adaugare eveniment pe buton
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isValid()){
+                utilizator.setNume(nume.getText().toString());
+                utilizator.setPrenume(prenume.getText().toString());
+                utilizator.setData(data.getText().toString());
+                utilizator.setEmail(email.getText().toString());
+                utilizator.setParola(parola.getText().toString());
+                Toast.makeText(MainActivity2.this,"Utilizator creat",Toast.LENGTH_LONG).show();}
+                //trimitere date incapsulate
+                Bundle bundle= new Bundle();
+
+                bundle.putParcelable("OK",utilizator);//ob parsabil
+                Intent intent=new Intent();
+                intent.putExtra("bundle",bundle);
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        });
+
+
+
+
+
     }
+    private boolean isValid(){
+        if(nume.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity2.this,"Nume invalid",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(prenume.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity2.this,"Prenume invalid",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(data.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity2.this,"Data invalida",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(email.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity2.this,"Email invalid",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if(parola.getText().toString().isEmpty()){
+            Toast.makeText(MainActivity2.this,"Parola invalida",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+    public boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
