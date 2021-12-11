f8f2157a46d46927f13d696fbaf4762fdde4e64b
MariaC <laviniacojocaru2000@gmail.com>
2021-12-11 18:56:11 +0200

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/youtube_tut/MainActivity.java
+++ app/src/main/java/com/example/youtube_tut/MainActivity.java
@@ -1,24 +1,25 @@
 package com.example.youtube_tut;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 private Button btn;
@@ -30,10 +31,12 @@ private UtilizatorDao utilizatorDAO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+       // writeToDatabase();
         setContentView(R.layout.activity_main);
+
         txt=findViewById(R.id.editTextTextEmailAddress2);
         btn=findViewById(R.id.bt1);//cauta butonul in
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,6 +76,8 @@ new Thread(new Runnable() {
         Log.v("operatii",lista3.toString());
     }
 }).start();
+        writeToDatabase();
+        readFromDatabase();
 
    }
 
@@ -91,5 +96,55 @@ new Thread(new Runnable() {
         }
     }
 
+public void writeToDatabase(){
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+    myRef.setValue("Hello, World!");
+    Utilizator u1=new Utilizator("Maria","Pop","12.09.2021","pop@gmail.com","oopo");
+myRef.child("Utilizator_1").setValue(u1);
+    Utilizator u2=new Utilizator("Ana","Lorena","12.02.2021","anaLorena@gmail.com","lll");
+    myRef.child("Utilizator_2").setValue(u2);
+    Utilizator u3=new Utilizator("Mihai","Ionut","28.07.20210","mihai.ionut@gmail.com","saposa");
+    myRef.child("Utilizator_3").setValue(u3);
+}
+
+public void readFromDatabase(){
+    // Read from the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    myRef.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+            //String value = dataSnapshot.getValue(String.class);
+           // Map<String, Object> value = (Map<String, Object>) dataSnapshot.getValue();
+            for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                String data = ds.child("data").getValue(String.class);
+                String email = ds.child("email").getValue(String.class);
+
+                String nume=ds.child("nume").getValue(String.class);
+                String prenume=ds.child("prenume").getValue(String.class);
+                String parola =ds.child("parola").getValue(String.class);
+                Log.d("citire",  data+ " , " + email+ " , "+parola + " , "+nume + " , "+prenume);
+            }
+            //Log.d("citire", "Value is: " + value);
+
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+            // Failed to read value
+            Log.w("Citire", "Failed to read value.", error.toException());
+
+
+        }
+
+
+    });
+
+}
 
 }
